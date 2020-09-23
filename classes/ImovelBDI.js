06583cc0e24f9bcb144aad50273136ab8e1870f0
lzd <ianlouzad4@gmail.com>
2020-09-23 19:34:48 -0300

    Interfaces para os bancos Imovel e Proprietario

---
+++ classes/ImovelBDI.js
@@ -0,0 +1,46 @@
+class ImovelDBI {
+    constructor() {
+        this.imoveis = [];
+    }
+
+    insereImovel(imovel) {
+        this.imoveis.push({...imovel});
+    }
+
+    //Recebe o id de um imóvel e tenta removê-lo, se um imóvel com aquele ID for encontrado e removido, a função retorna TRUE, caso contrário, FALSE
+    removeImovel(id) {
+        let wasRemoved = false;
+        for (let i = 0; i < this.imoveis.length; i++) {
+            if (this.imoveis[i].id == id) {
+                this.imoveis.splice(i, 1);
+                wasRemoved = true;
+                break;
+            }
+        }
+
+        return wasRemoved;
+    }
+
+    //Retorna o imóvel que com o id passado como argumento, returnua null caso não existe imóvel com o id passado
+    buscaImovelPorId(id) {
+        let imovel = null;
+
+        for (let i = 0; i < this.imoveis.length; i++) {
+            if (this.imoveis[i].id == id) {
+                imovel = { ...this.imoveis[i] };
+            }
+        }
+
+        return imovel;
+    }
+
+    //Retorna um array com todos os imóveis cadastrados
+    buscaImoveis() {
+        let imoveis = [];
+        for (let i = 0; i < this.imoveis.length; i++) {
+            imoveis.push({...this.imoveis[i]});
+        }
+
+        return imoveis;
+    }
+}
---
+++ classes/ProprietarioBDI.js
@@ -0,0 +1,46 @@
+class ProprietarioDBI {
+    constructor() {
+        this.proprietarios = [];
+    }
+
+    insereProprietario(proprietario) {
+        this.proprietarios.push({...proprietario});
+    }
+
+    //Se um proprietario com aquele ID for encontrado e removido, a função retorna TRUE, caso contrário, FALSE
+    removeProprietario(id) {
+        let wasRemoved = false;
+        for (let i = 0; i < this.proprietarios.length; i++) {
+            if (this.proprietarios[i].id == id) {
+                this.proprietarios.splice(i, 1);
+                wasRemoved = true;
+                break;
+            }
+        }
+
+        return wasRemoved;
+    }
+
+    //Retorna null caso não existe proprietario com o id passado
+    buscaProprietarioPorId(id) {
+        let proprietario = null;
+
+        for (let i = 0; i < this.proprietarios.length; i++) {
+            if (this.proprietarios[i].id == id) {
+                proprietario = { ...this.proprietarios[i] };
+            }
+        }
+
+        return proprietario;
+    }
+
+    //Retorna um array com todos os proprietarios cadastrados
+    buscaProprietarios() {
+        let proprietarios = [];
+        for (let i = 0; i < this.proprietarios.length; i++) {
+            proprietarios.push({...this.proprietarios[i]});
+        }
+
+        return proprietarios;
+    }
+}
